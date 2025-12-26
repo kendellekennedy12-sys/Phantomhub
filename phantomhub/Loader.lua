239ba68f56f6e65eb5b3b63cf200f1d908e07993
Phantomhub owner <kendellekennedy12@gmail.com>
2025-12-26 01:30:49 -0600

    Add game mappings to Loader.lua

--- phantomhub/Loader.lua
+++ phantomhub/Loader.lua
@@ -19,9 +19,16 @@ UI.SetStatus("Loading universal features...")
 safeLoad("Universal/Universal.lua")
 
 -- Game map
-local Games = {
-    -- Example:
-    -- [123456789] = "Games/123456789.lua",
+local Games = {local Games = {
+    [14259168147] = "Games/14259168147.lua", -- Basketball Legends
+    [8204899140] = "Games/8204899140.lua", -- Football Fusion
+    [18474291382] = "Games/18474291382.lua", -- Playground Basketball
+    [90913221847091] = "Games/90913221847091.lua", -- Football Legends
+    [91090484699204] = "Games/91090484699204.lua", -- Highschool Football
+    [2338325648] = "Games/2338325648.lua", -- Universe Football
+    [80681221431821] = "Games/80681221431821.lua", -- Practical Basketball
+    [17698425045] = "Games/17698425045.lua", -- Fight in a School
+    [99588440661442] = "Games/99588440661442.lua", -- Flag Football
 }
 
 UI.SetStatus("Checking game support...")

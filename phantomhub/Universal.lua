df810b17eb1a14a38940b95ff3c87c1f3b5bbde9
Phantomhub owner <kendellekennedy12@gmail.com>
2025-12-26 01:13:24 -0600

    Implement anti-AFK functionality in Universal.lua
    Added anti-AFK feature to Universal.lua.

--- phantomhub/Universal.lua
+++ phantomhub/Universal.lua
@@ -1,1 +1,12 @@
+-- Universal features (runs in ALL games)
+
+print("[PhantomHub] Universal loaded")
+
+-- Example anti-AFK
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    vu:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    task.wait(1)
+    vu:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+end)
 
